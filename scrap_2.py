a026df9c451563aef0b502b8dbd7d0ec2497c6b0
ghisford <ghislainnzima@gmail.com>
2022-10-14 21:59:29 +0300

    n0_2

--- scrap_2.py
+++ scrap_2.py
@@ -3,4 +3,31 @@
 #   points, comments, author, rank. The csv file should be sorted 
 #   by rank in ascending order.
 
-# https://news.ycombinator.com/ 
+from bs4 import BeautifulSoup
+import csv 
+import requests
+
+
+
+url ="https://news.ycombinator.com/"
+r = requests.get(url)
+
+# print(r.status_code)
+
+html_page = BeautifulSoup(r.content,'html.parser')
+# print(html_page.prettify())
+
+news_page = html_page.find('table',class_ = "itemlist")
+articles = news_page.find_all('tr',class_ = "athing")
+
+for article in articles:
+    if article == None:
+        continue
+    title = article.find('span',class_ = 'titleline').a.text
+    link =article.find('span',class_ = 'titleline').a['href']
+    points = article.find('span',class_ = "score").text
+    print(points)
+    exit()
+
+# print(news_page)d
+
